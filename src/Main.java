e90d497b65924f0811e9d5c9d10dbbbb6c76f328
Alena <umnovaalena@rambler.ru>
2023-03-09 21:20:48 +0400

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amound = 10_000_000;
+        int mile = 20;
+        int bonus = amound / mile;
+
+        System.out.println("Количество начисленных миль: " + bonus);
+    }
+}
